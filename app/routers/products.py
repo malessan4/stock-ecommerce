625e68a59587aed335e3da9d7e56d2264c58b5a0
malessan4 <matialess@gmail.com>
2026-01-07 15:26:23 -0300

    manejo de errores cuando tienen el mismo SKU

--- app/routers/products.py
+++ app/routers/products.py
@@ -1,21 +1,23 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session, select
+from sqlalchemy.exc import IntegrityError 
 from app.database import get_session
 from app.models import Product
 
-# Creamos el router (es como una mini-app de FastAPI)
 router = APIRouter(prefix="/products", tags=["products"])
 
-# 1. Endpoint para CREAR un producto (POST)
 @router.post("/", response_model=Product)
 def create_product(product: Product, session: Session = Depends(get_session)):
-    # 'session' es nuestra conexión a la DB inyectada automáticamente
-    session.add(product)      # Preparamos el producto para guardar
-    session.commit()          # Confirmamos el cambio en la DB
-    session.refresh(product)  # Recargamos el objeto con el ID generado por la DB
-    return product
+    try:
+        session.add(product)
+        session.commit()
+        session.refresh(product)
+        return product
+    except IntegrityError:
+        session.rollback() # Cancelamos la transacción para no dejar la DB trabada
+        # Devolvemos un error 400 (Bad Request) que el frontend entenderá
+        raise HTTPException(status_code=400, detail="El SKU ya existe, intenta con otro.")
 
-# 2. Endpoint para LISTAR todos los productos (GET)
 @router.get("/", response_model=list[Product])
 def read_products(session: Session = Depends(get_session)):
     products = session.exec(select(Product)).all()
